465340c19c486e666440f8d7999a17f4e57c2a6d
Casio <casiokobs12@gmail.com>
2022-06-29 09:02:31 -0300

    Função para puxar os dados que serao editados para o formulario de edit task

--- api-frontend/scripts/taskList.js
+++ api-frontend/scripts/taskList.js
@@ -6,13 +6,6 @@ async function delTask(_id){
     .catch(err => console.log(err))
 }
 
-
-async function editTask(_id){
-    //só redireciona pro id, nao funciona
-    window.location = 'http://127.0.0.1:3000/api/v1/task/'+_id 
-}
-
-
 async function getTask(){
     return await fetch("http://localhost:3000/api/v1/task").then(resposta=>{
         return resposta.json();
@@ -23,7 +16,7 @@ async function getTask(){
                                 <td>${values.description}</td>
                                 <td>${values.date}</td>
                                 <td>${validaNome(values.user)}</td>
-                                <td><button onclick=editTask('${values._id}')>Editar</button> <button id="delTask" onclick=delTask('${values._id}')>Remover</button></td>
+                                <td><button onclick=openFormEdit('${values._id}','${values.description}','${values.date}','${validaNome(values.user)}')>Editar</button> <button id='delTask' onclick=delTask('${values._id}')>Remover</button></td>
                             </tr>`
         });
         document.getElementById("tasks-table-content").innerHTML = tableData;
@@ -33,9 +26,31 @@ async function getTask(){
 }
 function validaNome(nome) {
     if (nome != null) {
+        console.log(nome.name);
         return nome.name
     }else{
-        return 'User deleted'
+        return 'User_Deleted'
     }
 }
 getTask();
+
+async function openFormEdit(_id, desc, data,user){
+
+    let popup = document.getElementById('popup');
+    let main = document.getElementById('main');
+    popup.classList.add('visible');
+    main.classList.add('hidden');
+
+    document.getElementById('user').value=user;
+    document.getElementById('description').value=desc;
+    document.getElementById('date').value=data;
+    document.getElementById('id').value=_id;
+
+
+    console.log(_id);
+    console.log(desc);
+    console.log(data);
+    console.log(user);
+    
+
+}
