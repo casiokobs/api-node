b20c444a7e09c78eca8de7178f5438c98ef613d0
Casio Kobs <casiokobs12@gmail.com>
2022-06-29 14:32:19 -0300

    permite a escolha do usuario no add task

--- api-frontend/scripts/taskAdd.js
+++ api-frontend/scripts/taskAdd.js
@@ -24,3 +24,18 @@ function alertMessage(a){
         window.location = 'index.html'; 
     }
 }
+async function populateSelect(){
+    return await fetch("http://localhost:3000/api/v1/user").then(resposta=>{
+        return resposta.json();
+    }).then(body =>{
+        let selectData = "";
+        body.map((values)=>{
+            selectData += `<option id="user" value=${values._id}>${values.name}</option>`
+        });
+        console.log(selectData);
+        document.getElementById("user").innerHTML = selectData;
+    }).catch(err =>{
+        console.log(err)
+    }) 
+}
+populateSelect();
